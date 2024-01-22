7548acb40fd1e1c46bb53323172f15c845d8cd31
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-01-22 06:07:56 -0500

    Add example _db.php file containing comment instructions for setting it up

---
+++ Config/_db.php
@@ -0,0 +1,13 @@
+<?php
+
+//change this file's name to db.php
+//and insert the host, port, dbname, username, and password 
+//needed to connect to your MySQL database
+
+return [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'ADD_DB_NAME',
+    'username' => 'ADD_DB_USER',
+    'password' => 'ADD_DB_PASSWORD',
+];
